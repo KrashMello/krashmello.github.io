d88428d389a59e2eb5ae461f07b8a34495b13a87
KrashMello <joelenrikesoteldo@gmail.com>
2023-07-10 11:22:32 -0400

    update with nuxt3

---
+++ _nuxt/_id.7eb8f63e.js
@@ -0,0 +1,1 @@
+import{_ as e,l as c,k as r}from"./entry.57ea0edd.js";const t={};function n(o,_){return r(),c("div")}const a=e(t,[["render",n]]);export{a as default};
---
+++ _nuxt/index.6eb9d919.js
@@ -0,0 +1,1 @@
+import{_ as e,l as c,k as n}from"./entry.57ea0edd.js";const r={};function t(o,s){return n(),c("div")}const a=e(r,[["render",t]]);export{a as default};
